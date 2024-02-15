70c073295df134c6fdbcafa6fdfc0584923b9368
David Francis <dm.francis@live.co.uk>
2024-02-15 20:46:16 +0000

    Updates to do with request efficiency and reducing duplicate http requests

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 // @ts-check
-async function main() {
+function main() {
 	const currentUrl = new URL(document.URL).pathname;
-
-	console.debug(currentUrl);
-
 	const actionUrls = ["/properties", "/property-for-sale", "/houses-for-sale", "/flats-for-sale"];
 
 	if (!actionUrls.some((url) => currentUrl.startsWith(url))) {
@@ -11,14 +8,14 @@ async function main() {
 		return;
 	}
 
-	await updatePropertiesWithFloorArea();
+	updatePropertiesWithFloorArea();
 
 	const mainElement = document.getElementById("content");
 	if (mainElement == null) {
 		throw new Error("No main content element found");
 	}
 
-	const observer = new MutationObserver(async() => await updatePropertiesWithFloorArea());
+	const observer = new MutationObserver(() => updatePropertiesWithFloorArea());
 	const obsConfig = { childList: true, subtree: true };
 	observer.observe(mainElement, obsConfig);
 }
@@ -26,16 +23,16 @@ async function main() {
 /**
  * Updates all properties with an extra icon showing floor area
  */
-async function updatePropertiesWithFloorArea() {
+function updatePropertiesWithFloorArea() {
 	const propertyElements = document.getElementsByClassName("propertyWrap");
 
-	const promises = [];
 	for (const element of propertyElements) {
-		promises.push(updatePropertyWithFloorArea(element));
+		updatePropertyWithFloorArea(element); // Note we are not awaiting any of these updates, they will all run on the event loop in the background
 	}
-
-	await Promise.all(promises);
 }
+
+// A global Set to keep track of which elements are being updated - keyed on url
+const lockedElements = new Set();
 
 /** Update a single property element with an icon showing floor area
  * @param {Element} element - The property Element to update
@@ -44,48 +41,62 @@ async function updatePropertyWithFloorArea(element) {
 	let alreadyAdded = element.querySelector(".added-floor-area");
 	if (alreadyAdded) { return; }
 
-	/** @type {HTMLAnchorElement | null} */
-	const aElement = element.querySelector(".infoWrap > a");
-	const url = aElement?.href;
+	const url = getUrlFromPropertyElement(element);
 	if (url == null) {
-		console.debug("No url found");
 		return;
 	}
 
-	const floorArea = await getFloorAreaFromPropertyPage(new URL(url));
-	if (!floorArea) {
-		console.debug("No floor area found");
+	// Ensure only 1 'thread' can update this property element at a time
+	if (lockedElements.has(url)) {
 		return;
 	}
+	lockedElements.add(url);
+
+	const floorArea = await getFloorAreaFromPropertyPage(url);
 
 	const infoDiv = element.getElementsByClassName("facilities")[0];
 	const newFloorAreaHtml = `<span class="opt added-floor-area">${floorArea}<span class="icon-floor_area" style="margin-left:8px"></span></span>`;
 	const floorAreaElement = createElementFromHtml(newFloorAreaHtml);
 	if (floorAreaElement == null) {
 		console.warn(`Invalid html string ${newFloorAreaHtml}`);
+		lockedElements.delete(url);
 		return;
 	}
 
-	alreadyAdded = element.querySelector(".added-floor-area"); // Check again in case this element is being updated by multiple threads.
-	if (alreadyAdded) { return; }
-
-	console.debug("Adding floor area to element");
 	infoDiv.appendChild(floorAreaElement);
+	lockedElements.delete(url);
+}
+
+/**
+ * Gets the url to the property page from a html element.
+ * @param {Element} element 
+ * @returns {string | null} - The url to the property page
+ */
+function getUrlFromPropertyElement(element) {
+	/** @type {HTMLAnchorElement | null} */
+	const aElement = element.querySelector(".infoWrap > a");
+	const rawUrl = aElement?.href;
+	if (rawUrl == null) {
+		return null;
+	}
+
+	return new URL(rawUrl).pathname;
 }
 
 /**
  * Fetches the property floor area from the given url.
- * @param {URL} url - The url of the property page.
+ * @param {string} url - The url of the property page.
  * @returns {Promise<string>}
  */
 async function getFloorAreaFromPropertyPage(url) {
-	const fromCache = sessionStorage.getItem(url.pathname);
-	if (fromCache) {
-		console.debug("Fetched floor area from cache");
+	const key = `floorarea-${url}`;
+	const fromCache = sessionStorage.getItem(key);
+	if (fromCache != null) {
 		return fromCache;
 	}
 
-	const response = await fetch(url.toString());
+	// Add a custom header to identify that this traffic comes from the extension
+	const response = await fetch(url, { credentials: "omit", headers: [["espc-floor-area-extension", "true"]]}); 
 	const html = await response.text();
 	const doc = new DOMParser().parseFromString(html, "text/html");
 
@@ -93,7 +104,7 @@ async function getFloorAreaFromPropertyPage(url) {
 	const floorAreaElement = doc.querySelector(".icon-floor_area + strong");
 	const floorAreaText = floorAreaElement?.innerText ?? "N/A";
 
-	sessionStorage.setItem(url.pathname, floorAreaText);
+	sessionStorage.setItem(key, floorAreaText);
 
 	return floorAreaText;
 }
