57b13d59c13c5a2f8446fca954ec03f082e29135
David Francis <dm.francis@live.co.uk>
2024-03-31 13:47:33 +0100

    Re-organise repo to allow separate artifacts for chrome and firefox

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+build_extension () {
+    local browser=$1
+    
+    rm -rf build/$browser
+    mkdir -p build/$browser
+
+    cp src/main.js build/$browser/
+    cp src/manifest-$browser.json build/$browser/manifest.json
+    cp icons/* build/$browser/
+
+    web-ext build --source-dir build/$browser --artifacts-dir build/$browser/artifacts --overwrite-dest
+}
+
+build_extension "firefox"
+build_extension "chrome"
